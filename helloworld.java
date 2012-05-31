022cafdcd021ab685344a3bfc32c840859f063b2
John Peterson <johnp@datalogics.com>
2012-05-31 13:40:22 -0500

    testing polling

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 /* Hello World Java Program */
 class helloworld  {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!);
        	}
 }
