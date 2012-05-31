c6c51bc154918492c3dad8393bd5a5ebf2cbf6f5
John Peterson <johnp@datalogics.com>
2012-05-31 13:33:16 -0500

    fix for test

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,6 @@
-/**
- * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
- */
-class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
+/* Hello World Java Program */
+class helloworld  {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+       	}
 }
-
